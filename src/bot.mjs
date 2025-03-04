c60246794803e33ee3a04e737d757a9b50bd96f5
Anna Radchuk <108876851+anyaradchuk87@users.noreply.github.com>
2025-03-04 21:59:56 +0000

    8

--- src/bot.mjs
+++ src/bot.mjs
@@ -2,7 +2,7 @@ import TeleBot from "telebot";
 
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN);
 
-bot.on("text", (msg) => msg.reply.text(msg.text));
+// bot.on("text", (msg) => msg.reply.text(msg.text));
 bot.on("/start", (msg) => {
   const chatId = msg.chat.id;
 
